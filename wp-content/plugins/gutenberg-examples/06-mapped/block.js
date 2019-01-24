debfc9313a3e88a0a98cbc1334ac79c1bcee9dbe
John Gulbronson <john@myfab5.com>
2019-01-24 00:04:26 -0500

    add map pin

--- wp-content/plugins/gutenberg-examples/06-mapped/block.js
+++ wp-content/plugins/gutenberg-examples/06-mapped/block.js
@@ -7,6 +7,32 @@
 	var ToggleControl     = components.ToggleControl; // For adding toggle controls to block settings panels.
 	var GOOGLE_API_KEY    = 'AIzaSyCHX3tZDcQNj7q0M7M0hoWPn_ooZ0-lxnE';
 
+	var updateLocation = _.debounce(function(location, setAttributes) {
+		console.log("UPDATING LOCATION: ", location);
+		$.ajax('https://maps.googleapis.com/maps/api/geocode/json', {
+			data: {
+				address: location,
+				key: GOOGLE_API_KEY
+			}
+		}).then(function(res) {
+			if(res.results && res.results[0]) {
+				var result = res.results[0];
+				if(result.geometry && result.geometry.location) {
+					let coordinates = result.geometry.location;
+					setAttributes({lat: coordinates.lat});
+					setAttributes({lng: coordinates.lng});
+				}
+			}
+		});
+	}, 500);
+
+	var esc = encodeURIComponent;
+	var buildURL = function(url, params) {
+		return url + '?' + Object.keys(params)
+	    .map(k => esc(k) + '=' + esc(params[k]))
+	    .join('&');
+	}
+
 	blocks.registerBlockType( 'gutenberg-examples/example-06-mapped', {
 		title: i18n.__( 'Map', 'gutenberg-examples' ),
 		description: 	i18n.__('Specify a city, address or place, and a map will be included'),
@@ -23,6 +49,16 @@
 				source: 'html',
 				selector: 'p',
 			},
+			lat: {
+				type: 'string',
+				source: 'meta',
+				meta: 'lat'
+			},
+			lng: {
+				type: 'string',
+				source: 'meta',
+				meta: 'lng'
+			}
 		},
 
 		edit: function( {attributes, setAttributes, focus, className}  ) {
@@ -32,6 +68,13 @@
 				description,
 			} = attributes;
 
+			function onChangeLatitude( event ) {
+	        props.setAttributes( { lat: event.target.value } );
+	    }
+
+			function onChangeLongitude( event ) {
+	        props.setAttributes( { lng: event.target.value } );
+	    }
 
 			return (
 				el( 'div', { className: className },
@@ -42,6 +85,8 @@
 						value: attributes.location,
 						onChange: function( value ) {
 							setAttributes( { location: value } );
+							updateLocation.cancel();
+							updateLocation(value, setAttributes);
 						},
 					} ),
 					el( RichText, {
@@ -52,14 +97,32 @@
 						onChange: function( value ) {
 							setAttributes( { description: value } );
 						},
-					} )
+					} ),
+					el('div', {}, i18n.__( 'Latitude', 'gutenberg-examples' ) ),
+					el('input', {
+						value: attributes.lat,
+						onChange: onChangeLatitude,
+					}),
+					el('div', {}, i18n.__( 'Longitude', 'gutenberg-examples' ) ),
+					el('input', {
+						value: attributes.lng,
+						onChange: onChangeLongitude,
+					}),
 				)
 			);
 		},
 		save: function( props ) {
 			var attributes = props.attributes;
-			//
-			var mapURL = 'https://maps.googleapis.com/maps/api/staticmap?center=' + attributes.location + '&zoom=13&size=600x300&maptype=roadmap&key=' + GOOGLE_API_KEY;
+
+			// Create google static maps URL
+			var mapURL = buildURL('https://maps.googleapis.com/maps/api/staticmap', {
+				center: attributes.location,
+				zoom: 13,
+				size: '600x300',
+				maptype: 'roadmap',
+				markers: attributes.lat + ',' + attributes.lng,
+				key: GOOGLE_API_KEY,
+			});
 			return (
 				el( 'div', { className: props.className },
 					el( RichText.Content, {
